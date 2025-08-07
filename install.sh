165b557524e56dc9c94da5194ba74ffdb2df8322
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-07 20:45:43 +0530

    creating service account

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/
 gpgcheck=0
 enabled=1' > /etc/yum.repos.d/mongo.repo
 
-yum install -y mongodb-org-shell
+yum install -y mongodb-org-shell -y
 
