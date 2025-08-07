ced10cc5ebb8d046803e25ecac2aafbfe3dfb6ba
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-08 00:16:13 +0530

    creating service account

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
 FROM amazonlinux:2
 
-# Enable Amazon Linux extras for MongoDB and install required packages
-RUN amazon-linux-extras enable corretto8 && \
-    yum install -y \
-        mongodb-org-shell \
-        mysql \
-        git \
-        awscli && \
-    yum clean all
+# Install MongoDB Shell, MySQL, Git, AWS CLI
+RUN yum install -y \
+    amazon-linux-extras \
+    && amazon-linux-extras enable corretto8 \
+    && yum install -y \
+       mongodb-org-shell \
+       mysql \
+       git \
+       awscli \
+    && yum clean all
 
 WORKDIR /app
 
-COPY install.sh /
-COPY run.sh /
-
-RUN chmod +x /install.sh /run.sh
+COPY run.sh /run.sh
+RUN chmod +x /run.sh
 
 ENTRYPOINT ["/run.sh"]
 
--- run.sh
+++ run.sh
@@ -2,31 +2,21 @@
 set -e
 set -x
 
-mkdir -p /parameters
-> /parameters/params
-
-# Fetch parameters from SSM and export to /parameters/params
-for param in $PARAMS ; do
-  PARAM=$(echo $param | awk -F, '{print $1}')
-  KEY=$(echo $param | awk -F, '{print $2}')
-  VALUE=$(aws ssm get-parameter --name $PARAM --region us-east-1 --with-decryption --query "Parameter.Value" --output text)
-  echo "export $KEY=\"$VALUE\"" >> /parameters/params
-done
-
-# Source parameters
+# Load parameters from init container
 source /parameters/params
 
-# Clone your app repo
+# Clone schema repo
 git clone https://github.com/teju-devopss/learn-kubernetes.git /app
 
-cd /app/schema/${component}
+cd /app/schema/${COMPONENT}
 
-# Load schema
-if [ "$db" == "mongo" ]; then
-  mongo --host ${MONGO_ENDPOINT} < ${component}.js
-elif [ "$db" == "mysql" ]; then
-  mysql -h ${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < /app/schema/${component}/shipping.sql
+# Load schema based on SCHEMA_TYPE
+if [ "$SCHEMA_TYPE" == "mongo" ]; then
+  mongo --host ${DOCDB_ENDPOINT} -u ${DOCDB_USER} -p ${DOCDB_PASS} < ${COMPONENT}.js
+elif [ "$SCHEMA_TYPE" == "mysql" ]; then
+  mysql -h ${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} < ${COMPONENT}.sql
 else
-  echo "Unsupported DB: $db"
+  echo "Unsupported schema type: $SCHEMA_TYPE"
   exit 1
 fi
+
