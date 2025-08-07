2e01d033e2b55925be61f91fb3f431824c48c60c
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-07 23:37:41 +0530

    creating service account

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=0
 enabled=1' >/etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org-shell mysql git -y
+yum install mongodb-org-shell mysql git -y
 
