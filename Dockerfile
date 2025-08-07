8a755ad1df7ab7cb54a7a29e3acfe7b16ef01620
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-07 23:18:32 +0530

    creating service account

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ FROM amazonlinux:2
 RUN yum install -y yum-utils curl git mysql && yum clean all
 
 # Copy and execute install.sh
-COPY install.sh /
-RUN bash /install.sh
+COPY          install.sh /
+RUN           bash /install.sh
 
 # Copy and set entrypoint
-COPY run.sh /
+COPY           run.sh /
 ENTRYPOINT ["bash", "/run.sh"]
 
--- run.sh
+++ run.sh
@@ -1,38 +1,18 @@
-##We assume we get the following variables from K8S
-##COMPONENT
-##SCHEMA_TYPE
-#
-#mkdir /app
-#cd /app
-#git clone https://github.com/teju-devopss/$COMPONENT .
-#
-#source /parameters/params
-#
-#if [ "$SCHEMA_TYPE" == "mongo" ]; then
-#curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-#mongosh $DOCDB_ENDPOINT:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username $DOCDB_USER --password $DOCDB_PASS </app/schema/$COMPONENT.js
-#fi
-#
-#if [ "$SCHEMA_TYPE" == "mysql" ]; then
-#mysql -h $DB_HOST -u$DOCDB_USER -p$DOCDB_PASS </app/schema/$COMPONENT.sql
-#fi
+# We assume we get the following variables from K8S
+# COMPONENT
+# SCHEMA_TYPE
 
-
-#!/bin/bash
-
-# Clone component code
-mkdir -p /app
+mkdir /app
 cd /app
 git clone https://github.com/teju-devopss/$COMPONENT .
 
-# Load environment variables
 source /parameters/params
 
 if [ "$SCHEMA_TYPE" == "mongo" ]; then
   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo $DOCDB_ENDPOINT:27017 --ssl --sslCAFile global-bundle.pem --username $DOCDB_USER --password $DOCDB_PASS </app/schema/$COMPONENT.js
+  mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile global-bundle.pem --username $DOCDB_USER --password $DOCDB_PASS < /app/schema/$COMPONENT.js
 fi
 
 if [ "$SCHEMA_TYPE" == "mysql" ]; then
-  mysql -h $DB_HOST -u$DOCDB_USER -p$DOCDB_PASS </app/schema/$COMPONENT.sql
+  mysql -h $DB_HOST -u$DB_USER -p$DB_PASS </app/schema/$COMPONENT.sql
 fi
