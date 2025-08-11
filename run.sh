8dd6b38ef414112df9e3b3d9b4516f2b21294374
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-11 17:47:10 +0530

    creating service account

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ source /parameters/params
 if [ "$SCHEMA_TYPE" == "mongo" ]; then
   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
 #  mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile global-bundle.pem --username $DOCDB_USER --password $DOCDB_PASS < /app/schema/$COMPONENT.js
-  mongo --tls --host ${DOCDB_ENDPOINT}:27017 --tlsCAFile global-bundle.pem --username ${DOCDB_USER} --password $DOCDB_PASS </app/schema/${COMPONENT}.js
+  mongo --tls --host $DOCDB_ENDPOINT:27017 --tlsCAFile global-bundle.pem --username $DOCDB_USER --password $DOCDB_PASS </app/schema/${COMPONENT}.js
 fi
 
 if [ "$SCHEMA_TYPE" == "mysql" ]; then
